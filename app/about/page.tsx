22f53222f1ffe8d8e6ce8bbd07732e266cefc0cd
Peggy A. Roxy <79624453+Drpearlz@users.noreply.github.com>
2025-04-12 12:03:06 -0700

    Update page.tsx with "

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -51,8 +51,7 @@ export default function AboutUs() {
       age: 5-10,
       image: "/child1-placeholder.jpg", // Replace with actual image path
       description:
-        "ChiLiveth is a fourth-grader who loves science experiments and ballet. She is excited to be a big sister again and has years of experience with her brother. Her favorite things are playing the piano, crochetting, and playing with my squishy doughs.
-        For Chimenem he enjoys Gecko (PJ Masks), Spidey and Chase (Paw Patrol)..",
+        "ChiLiveth is a fourth-grader who loves science experiments and ballet. She is excited to be a big sister again and has years of experience with her brother. Her favorite things are playing the piano, crochetting, and playing with my squishy doughs. For Chimenem he enjoys Gecko (PJ Masks), Spidey and Chase (Paw Patrol).",
       funFacts: [
         "Chimenem is the best lawyer in the family.",
         "Both birthdays are in July, a week apart",
